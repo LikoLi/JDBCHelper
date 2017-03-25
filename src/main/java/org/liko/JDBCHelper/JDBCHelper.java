895f293cd59ac2349a4a22c09abc4400b752c31b
lkzw <715471748@qq.com>
2017-03-25 16:50:22 +0800

    add callableQuery

--- src/main/java/org/liko/JDBCHelper/JDBCHelper.java
+++ src/main/java/org/liko/JDBCHelper/JDBCHelper.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -135,7 +136,120 @@ public class JDBCHelper {
 			release(con, preparedStatement, rs);
 		}
 	}
+	
+	public static List callableQuery(String procedureSql) throws SQLException {
+		ResultSet rs = null;
+		try {
+			getCallableStatement(procedureSql);
+			rs = callableStatement.executeQuery();
+			return resultToListMap(rs);
+		} finally {
+			release(rs, callableStatement);
+		}
+	}
+	
+	public static List callableQuery(String procedureSql, Object... paramters) throws SQLException {
+		ResultSet rs = null;
+		try {
+			getCallableStatement(procedureSql);
+			for(int i = 0; i < paramters.length; i++) {
+				callableStatement.setObject(i + 1, paramters[i]);
+			}
+			rs = callableStatement.executeQuery();
+			return resultToListMap(rs);
+		} finally {
+			release(rs, callableStatement);
+		}
+	}
+	
+	public static Object callableQuerySingle(String procedureSql) throws SQLException {
+		Object result = null;
+		ResultSet rs = null;
+		try {
+			getCallableStatement(procedureSql);
+			rs = callableStatement.executeQuery();
+			while(rs.next()) {
+				result = rs.getObject(1);
+			}
+			return result;
+		} finally {
+			release(rs, callableStatement);
+		}
+	}
+	
+	public static Object callableQuerySingle(String procedureSql, Object... paramters) throws SQLException {
+		Object result = null;
+		ResultSet rs = null;
+		try {
+			getCallableStatement(procedureSql);
+			for(int i = 0; i < paramters.length; i++) {
+				callableStatement.setObject(i + 1, paramters[i]);
+			}
+			rs = callableStatement.executeQuery();
+			while(rs.next()) {
+				result = rs.getObject(1);
+			}
+			return result;
+		} finally {
+			release(rs, callableStatement);
+		}
+	}
+	
+	public static Object callableWithParamters(String procedureSql) throws SQLException {
+		try {
+			getCallableStatement(procedureSql);
+			callableStatement.registerOutParameter(0, Types.OTHER);
+			callableStatement.execute();
+			return callableStatement.getObject(0);
+		} finally {
+			release(null, callableStatement);
+		}
+	}
+	
+	public static int callableUpdate(String procedureSql) throws SQLException {
+		try {
+			getCallableStatement(procedureSql);
+			return callableStatement.executeUpdate();
+		} finally {
+			release(null, callableStatement);
+		}
+	}
+	
+	public static int callableUpdate(String procedureSql, Object... parameters) throws SQLException {
+		try {
+			getCallableStatement(procedureSql);
+			for(int i = 0; i < parameters.length; i++) {
+				callableStatement.setObject(i + 1, parameters[i]);
+			}
+			return callableStatement.executeUpdate();
+		} finally {
+			release(null, callableStatement);
+		}
+	}
+	
+	public static int[] batchUpdate(List<String> sqlList) throws SQLException {
+		int[] result = new int[]{};
+		Statement statement = null;
+		try {
+			con = JDBCUtil.getConnection();
+			con.setAutoCommit(false);
+			statement = con.createStatement();
+			for(String sql : sqlList) {
+				statement.addBatch(sql);
+			}
+			result = statement.executeBatch();
+			con.commit();
+		} finally {
+			release(con, statement, null);
+		}
+		return result;
+	}
 
+
+	private static void getCallableStatement(String procedureSql) throws SQLException {
+		con = JDBCUtil.getConnection();
+		callableStatement = con.prepareCall(procedureSql);
+	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private static List resultToListMap(ResultSet rs) throws SQLException {
@@ -161,7 +275,11 @@ public class JDBCHelper {
 	}
 	
 	private static void release(ResultSet rs) throws SQLException {
-		JDBCUtil.release(con, preparedStatement, rs);
+		release(con, preparedStatement, rs);
+	}
+	
+	private static void release(ResultSet rs, Statement statement) throws SQLException {
+		release(con, statement, rs);
 	}
 	
 	private static void release(Connection con, Statement statement, ResultSet rs) throws SQLException {
